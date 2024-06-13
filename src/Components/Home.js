68df862f28fb4885371fae839e656cdce088ed95
Sai Dithvika <saidithvika@gmail.com>
2024-06-13 13:08:46 +0530

    dashboard done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../Components/home.css'
 import MonthlyReport from './MonthlyReport'
 import FormSubmit from './FormSubmit'
+import Bouncerate from './Bouncerate'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
     <div className='dashboard'>
     <MonthlyReport />
     <FormSubmit />
+    <Bouncerate />
     </div>
     </>
   )
