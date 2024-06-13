bed024c519679d6f763c3e4c97746ff8df1d53e8
Catherinejessie-04 <124045353+Catherinejessie-04@users.noreply.github.com>
2024-06-13 13:18:48 +0530

    Map Added

--- src/Components/WorldMap.js
+++ src/Components/WorldMap.js
@@ -1,13 +1,54 @@
-import { usMap } from './usa-map';
-import React from 'react';
-import { MapsComponent, LayersDirective, LayerDirective } from '@syncfusion/ej2-react-maps';
-export function WorldMap() {
- return (<MapsComponent>
-          <LayersDirective>
-             <LayerDirective shapeData={usMap}>
-             </LayerDirective>
-          </LayersDirective>
-    </MapsComponent>);
- }
+import React ,{useEffect,useState} from "react";
+import { Chart } from "react-google-charts";
+import axios from 'axios';
+import getYearFirst from '../Helpers/getYearFirst';
 
-export default WorldMap;
+
+export const options = {
+    // region: "034", // India
+    colorAxis: { colors: ["#00853f", "black", "#e31b23"] },
+    backgroundColor: "#81d4fa",
+    datalessRegionColor: "#f8bbd0",
+    defaultColor: "#f5f5f5",
+  };
+const mapper = (data)=>{
+    var id=0;
+    var res =[
+        ["City", "Users"],
+    ];
+    for(let i of data){
+        res.push([i.label1,parseInt(i.value1)]);
+        id++;
+    }
+    return res;
+}
+
+function WorldMap() {
+    const [data, setData] = useState([]);
+    const date = getYearFirst();
+    useEffect(() => {
+        axios
+          .post("http://localhost:3000/analytics/report", {
+            dimensions: ["country"],
+            metrics: ["totalUsers"],
+            dateRanges: [[date, "today"]],
+          })
+          .then((response) => {
+            setData(mapper(response.data.data));
+            console.log(data);
+          });
+    }, []);
+    return (
+        <Chart
+          
+          chartType="GeoChart"
+          width="100%"
+          height="400px"
+          data={data}
+          options={options}
+        />
+      );
+    }
+    
+
+export default WorldMap
