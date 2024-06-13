88a0ff63f1d8cde3f72c0394830010d5efc9586f
Catherinejessie-04 <124045353+Catherinejessie-04@users.noreply.github.com>
2024-06-13 10:25:22 +0530

    bounce rate completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home'
 import MonthlyReport from './Components/MonthlyReport';
+import Bouncerate from './Components/Bouncerate';
+import WorldMap from './Components/WorldMap';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/monthly-report" element={<MonthlyReport />} />
+          <Route path="/bounce-rate" element={<Bouncerate/>}/>
+          <Route path="/map" element={<WorldMap/>}/>
         </Routes>
       </BrowserRouter>
     </div>
